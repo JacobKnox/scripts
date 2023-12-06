b3e7b6513306e9f59c19a78d27e6e39d23cafec1
Jacob Knox <epicdude203@gmail.com>
2023-12-06 15:34:25 -0500

    Add installer script
    Add small script I wrote to install winget (https://winget.run/) packages given a text file of package names (programs.txt)

---
+++ Installer/installer.cmd
@@ -0,0 +1,13 @@
+@echo off
+title Installer Script
+echo You may be required to interact with the console or installation windows
+pause
+echo Going to root directory...
+set startpath=%cd%
+cd C:\
+FOR /F "tokens=*" %%i IN (%startpath%\programs.txt) DO (
+    echo Installing %%i...
+    winget install "%%i"
+)
+echo Returning to previous directory...
+cd %startpath%
